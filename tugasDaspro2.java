ae0be459ba43cd55d14cf974db3f68929e61dbad
MeutiaArafah <meutiaarafah@gmail.com>
2024-09-06 15:40:03 +0700

    commitnya meutiaa

---
+++ tugasDaspro2.java
@@ -0,0 +1,9 @@
+
+public class tugasDaspro2 {
+
+    public static void main(String[] args) {
+        System.out.println("Nama      : Meutia Arafah Hidayat");
+        System.out.println("NIM       : 244107060078");
+        System.out.println("Alamat    : Jl. Danau Sentani Tengah VI");
+    }
+}
